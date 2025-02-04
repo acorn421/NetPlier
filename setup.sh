c8cfb27c0db79169f0f790de60826b7dd0b5c1ef
acorn421 <acorn421@gmail.com>
2025-02-05 01:19:46 +0900

    feat: update setup.sh to install mafft and add patching instructions

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-pyenv isntall 3.6
+# Install mafft
+sudo apt-get install mafft
+
+# Install python 3.6 and make a virtual environment for it
+pyenv install 3.6
 
 virtualenv -p ~/.pyenv/versions/3.6.12/bin/python3.6 .venv3.6
 source .venv3.6/bin/activate
@@ -7,8 +11,15 @@ curl -O https://bootstrap.pypa.io/pip/3.6/get-pip.py
 python get-pip.py
 rm -rf get-pip.py
 
+# Install python packages
 pip install --upgrade --force-reinstall setuptools
 pip install numpy==1.19.5
 pip install cython
 pip install impacket
 pip install -r requirements.txt
+
+# Patching NetZob
+patch -p1 < netzob.patch
+
+# Run test command
+# python netplier/main.py -i data/dhcp_100.pcap -o tmp/dhcp -t dhcp 
